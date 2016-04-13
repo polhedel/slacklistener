3116a37a0874f6efb2b0a6f840920c094d89c89d
polhedel <polhedel@gmail.com>
2016-04-13 19:09:06 +0200

    Added files via upload

--- testing/testingslackclient.js
+++ testing/testingslackclient.js
@@ -2,14 +2,25 @@
 var common = require ('../common/common');
 var util = require('util');
 
+var proxiedRequestTransport = require('@slack/client/lib/clients/transports/request.js').proxiedRequestTransport;
+var wsTransport = require('@slack/client/lib/clients/transports/ws');
+
 var RtmClient = require('@slack/client').RtmClient;
 var token = common.config().slack_token || '';
 
 var rtm = new RtmClient(
   common.config().slack_token || '',
   {
-    logLevel: common.config().loglevel,
-    proxyUrl: 'http://10.110.8.42:8080'
+    logLevel: 'debug',
+    transport: proxiedRequestTransport('http://10.110.8.42:8080'),
+    socketFn: function(socketUrl) {
+        return wsTransport(socketUrl, {
+            proxyURL: 'http://10.110.8.42:8080',
+            // there's a mistake in transport/ws.js that tries to use both "proxyURL" and "proxyUrl"
+            // so just submit both options for now as a temporary workaround.
+            proxyUrl: 'http://10.110.8.42:8080'
+        });
+    }
   }
 );
 
